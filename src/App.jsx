c8e000dd6876ec6d0c5d476882d2431660fe0007
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-11 22:59:34 -0700

    data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Blogs from './Componentes/Blogs/Blogs'
+import Header from './Componentes/Header/Header'
 function App() {
   
   return (
     <>
-     
-      <h1 className='text-4xl'>Knowledge Cafe</h1>
-      
+
+      <Header></Header>
+      <Blogs></Blogs>
+
     </>
   )
 }
