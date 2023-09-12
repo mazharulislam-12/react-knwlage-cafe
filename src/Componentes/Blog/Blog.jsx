89c1fbc0d777ab8275c8aee494225186c3b597b6
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-12 02:39:27 -0700

    json to data load and show this data

--- src/Componentes/Blog/Blog.jsx
+++ src/Componentes/Blog/Blog.jsx
@@ -1,9 +1,31 @@
 import PropTypes from 'prop-types'; 
+import { FaBookmark } from 'react-icons/fa';
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover, reading_time, author, author_img, posted_date, hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-8 rounded-md' src= {cover} alt= {`cover picture of the title ${title}`} />
+
+            <div className='flex justify-between'>
+                <div className='flex gap-6'>
+                    <img className='w-14' src= {author_img} alt="" />
+                    <div>
+                        <h2 className='text-2xl mb-2'>{author}</h2>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <span>{reading_time} min red</span>
+                    <button className='ml-4 text-xl text-red-600'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-2xl mt-4 font-bold mb-2">{title}</h2>
+        <p>
+            {
+                hashtags.map((hash, idx) => <span key={idx}><a href=""># {hash} </a></span>)
+            }
+        </p>
         </div>
     );
 };
